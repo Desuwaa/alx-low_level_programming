bbc6d84fe44c16a41f2288ee65a0ce058c8e34f0
Desuwaa <airekpita@gmail.com>
2023-03-17 13:29:47 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - prints numbers from 0 t0 9
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar ('\n');
+	return (0);
+}
