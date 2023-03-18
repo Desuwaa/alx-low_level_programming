95b9da9e0fcfbfeebcaaffd6dc722b5af7688b3e
Desuwaa <airekpita@gmail.com>
2023-03-17 17:15:30 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,25 +5,23 @@
  */
 int main(void)
 {
-	int i, j;
+	int p, q;
 
-	for (i = 0; i < 100; i++)
-		for (j = 0; j < 100; j++)
-			if (i < j)
-			{
-				if (i < j)
-				{
-					putchar ((i / 10) + 48);
-					putchar ((i % 10) + 48);
-					putchar ((j / 10) + 48);
-					putchar ((j % 10) + 48);
-					if (i != 98 || j != 99)
-					{
-						putchar (',');
-						putchar (' ');
-					}
-				}
-			}
-	putchar ('\n');
+	for (p = 0; p <= 98; p++)
+	{
+		for (q = p + 1; q <= 99; q++)
+		{
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
+			if (p == 98 && q == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 	return (0);
 }
