9825d73e29d8d7260a6876139c099e5e3ec984ef
Desuwaa <airekpita@gmail.com>
2023-04-05 20:55:31 -0700

    0-main.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints string recursively
+ * @s: a string of character
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 2);
+	} else
+	{
+	    putchar("\n");
+	}
+}
