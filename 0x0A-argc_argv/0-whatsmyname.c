fe912de0325dcbce305ae07c3ad5682dcddbaf7a
Desuwaa <airekpita@gmail.com>
2023-04-10 00:37:19 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * name - a prgram that prints its name
+ * @argc: arguement counter
+ * @argv: array of pointers to arguements
+ *
+ * Return: returns 0
+ */
+int name(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
