e0fb80841bb2de9e2fbbac18def01b15e4cb4532
Desuwaa <airekpita@gmail.com>
2023-04-29 16:29:06 -0700

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+  * print_listint_safe - define function
+  * @head: indicates parameter name
+  * Return: the number of nodes in the list
+  */
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}
